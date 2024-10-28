13a4042b85d60e2ae0e963e27c9452433aa91bab
Dor <madebydor@gmail.com>
2024-10-28 23:45:53 +0200

    uhhhhh

--- src/app/(auth)/login/google/route.ts
+++ src/app/(auth)/login/google/route.ts
@@ -1,43 +1,7 @@
-"use server";
-import { google } from "@/lib/server/oauth";
-import { globalGETRateLimit } from "@/lib/server/request";
-import { generateCodeVerifier, generateState } from "arctic";
-import { cookies } from "next/headers";
+
+import { NextResponse } from "next/server";
 
 export async function GET(): Promise<Response> {
-  if (!globalGETRateLimit()) {
-    return new Response("Too many requests", {
-      status: 429,
-    });
-  }
-  const cookiesData = await cookies();
-  const state = generateState();
-  const codeVerifier = generateCodeVerifier();
-  const url = google.createAuthorizationURL(state, codeVerifier, [
-    "openid",
-    "profile",
-    "email",
-  ]);
+  return NextResponse.json({ message: "Hello, world!" });
 
-  cookiesData.set("google_oauth_state", state, {
-    path: "/",
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 60 * 10, // 10 minutes
-    sameSite: "lax",
-  });
-  cookiesData.set("google_code_verifier", codeVerifier, {
-    path: "/",
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    maxAge: 60 * 10, // 10 minutes
-    sameSite: "lax",
-  });
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: url.toString(),
-    },
-  });
 }
